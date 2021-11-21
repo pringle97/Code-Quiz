58193774ede8034ddd1c1f8454b7b83d08df840b
pringle97 <malia.pringle@gmail.com>
2021-11-21 01:53:59 -0800

    commiot

--- script.js
+++ script.js
@@ -142,24 +142,27 @@ function endQuiz() {
   question.classList.add('hide')
 }
 
-const submitScore = document.getElementById('submitScore') 
-  document.getElementById('submitScore').addEventListener('click', event => {
-    event.preventDefault()
-    // let usernames = document.getElementById('usernames')
-    localStorage.setItem('usernames', usernames.value)
-    localStorage.setItem('scores', timeleft)
-    let displayScores = document.getElementById('displayScores')
-    let highscoreElem = document.createElement(`div`)
-    let scores = JSON.parse(localStorage.getItem('scores')) || []
-    let usernames = JSON.parse(localStorage.getItem('usernames')) || []
-    highscore.innerHTML = `
+const submitScore = document.getElementById('submitScore')
+document.getElementById('submitScore').addEventListener('click', event => {
+  event.preventDefault()
+  // let usernames = document.getElementById('usernames')
+  localStorage.setItem('usernames', usernames)
+  localStorage.setItem('scores', timeleft)
+  let displayScores = document.getElementById('displayScores')
+  let highscoreElem = document.createElement(`div`)
+  let scores = JSON.parse(localStorage.getItem('scores')) || []
+  let usernames = JSON.parse(localStorage.getItem('usernames')) || []
+  highscore.innerHTML = `
     <h1>${usernames.value}</h1>
     <h1>${scores}</h1>
     `
-    scores.push(new score)
-    localStorage.setItem('scores', scores) // overwrite old scores with new scores
-    displayScores.classList.remove('hide')
-    })
-  
+  scores.push({
+    usernames: usernames.value,
+    scores: scores
+  })
+  localStorage.setItem('scores', scores) // overwrite old scores with new scores
+  displayScores.classList.remove('hide')
+})
+
 
 
